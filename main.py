83d48c03b0b60c3c86a619d789ab1a50825d7db4
brenno16 <brenno1413@hotmail.com>
2023-03-02 00:58:21 -0300

    Testes usando unittest

---
+++ main.py
@@ -0,0 +1,16 @@
+# funçao de adição
+def add(n1, n2):
+    return n1 + n2
+
+# função de subtração
+def sub(n1, n2):
+    return n1 - n2
+
+# função de multiplicação
+def mult(n1, n2):
+    return n1 * n2
+
+# função de divisão
+def div (n1,n2):
+    return n1 / n2
+
